8e227ccd56d628396897569d3b8fa3ef5f1f9d91
amal123achu <132316883+amal123achu@users.noreply.github.com>
2023-08-05 14:17:32 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,48 @@
+<html>
+    <head>
+    <link type="text/css" rel="stylesheet" href="style2.css">
+    <style>
+        .background{
+    position: absolute;
+    height:100%;
+    width:100%;
+    background-image:url("https://wallpaperaccess.com/full/4722387.jpg");
+    background-size:cover;
+    background-position: center;
+    filter: blur(2px);
+    z-index: -1;
+}
+p{
+    background-color: cornsilk;
+    width:70%;
+
+}
+    </style>
+</head>
+    <body >
+    <div class="background"></div>   
+    
+    <form action="home1.php" method="post">
+    <?php
+        session_start();
+        echo $_SESSION["uid"];
+       
+        ?>
+        
+        <h1><b><u>Anytime Fitness</u></b></h1>            
+        
+        <div class="div1">
+        <input class="block" type="submit" name="profile" value="PROFILE">
+        <input class="block" type="submit" name="pay" value="PAYMENT">
+        <input class="block" type="submit" name="trainer" value="TRAINER">
+        <input class="block" type="submit" name="logout" value="LOGOUT">
+        </div>
+       
+        <h3><b>WELCOME</b></h3>
+        <br><br><br><br><br><br><br><br>
+        <p><b>Anytime Fitness<br>Cherpulassery<br></b></p>
+       
+    </form>
+    </body>
+</html>
+
